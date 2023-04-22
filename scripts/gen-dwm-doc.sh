3ed8487bab575de21c10b897293f8fbad7053d10
robtekky <robtekky@gmail.com>
2023-04-22 10:29:17 +0200

    Remove all tmp files generated previously no matter what.

--- scripts/gen-dwm-doc.sh
+++ scripts/gen-dwm-doc.sh
@@ -40,7 +40,7 @@
 path='/tmp'
 config_file=~/git/dwm/config.h
 # Remove any previous tmp file
-[ "$WM_HELP_LAUNCHER_REMOVE_TMP_INLINE" ] || rm $path/dwm-keybindings-*.html 2>/dev/null
+[ "$WM_HELP_LAUNCHER_REMOVE_TMP_INLINE" ] || rm $path/dwm-keybindings-* 2>/dev/null
 tmp_file=$(mktemp --suffix=.org ${path}/dwm-keybindings-XXXX)
 
 marker='^\s*/\*d\*\s+'  # Used to identify comments including keybindings info in config.h
